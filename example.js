29afcf9881dde0ed145cba18ddb54dfa622b3c32
tonn24 <tonn.toomsalu@gmail.com>
2020-01-25 12:45:04 +0200

    First commit

---
+++ example.js
@@ -0,0 +1,26 @@
+/* class Lammas() {
+    name = "";
+
+    constructor(name) {
+        this.name = name;
+    }
+
+    name(params) {
+        return this.name;
+    }
+    
+    setName(name) {
+        this.name = name;
+    }
+} */
+
+/* const arr = [1, 2, "hey", "yo"];
+
+arr.forEach(function(el, i) {
+    console.log("---" + el);
+});
+ */
+
+ 
+
+
